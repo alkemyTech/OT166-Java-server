b0f471ac759bc5febeae0dcda23623cd285f9c47
Magali Kain <magali.kain@thoughtworks.com>
2022-03-11 23:25:27 +0100

    Define software architecture

---
+++ src/main/java/com/alkemy/ong/infrastucture/spring/config/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.alkemy.ong.infrastucture.spring.config.security;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().anyRequest().permitAll();
+  }
+
+}
