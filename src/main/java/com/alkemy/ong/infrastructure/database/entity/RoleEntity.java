8acecd963d5ee378d4c4e73158d57f79e014545b
devfacu <facucba@gmail.com>
2022-03-17 19:21:02 -0300

    OT166-16: Include model and repository for Roles (#3)

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/RoleEntity.java
@@ -0,0 +1,39 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "ROLES")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ROLE_ID")
+  private Long id;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @Column(name = "DESCRIPTION")
+  private String description;
+
+  @CreationTimestamp
+  @Column(name = "CREATE_TIMESTAMP")
+  private Timestamp createTimestamp;
+
+}
