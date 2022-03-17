c217226494e6753adf25dd4ad4f0b9fe1c50f9d6
Matias <unity.matias@outlook.com>
2022-03-17 14:39:55 -0300

    Add JWT logic

---
+++ src/main/java/com/alkemy/ong/infrastructure/spring/config/security/UserDetailsCustomService.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.infrastructure.spring.config.security;
+
+import java.util.ArrayList;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsCustomService implements UserDetailsService {
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return new User("root", "pass123", new ArrayList<>());
+  }
+}
