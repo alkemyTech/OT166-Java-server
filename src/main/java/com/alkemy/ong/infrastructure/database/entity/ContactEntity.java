7ae6441bdd1e3f382cd9ed810e653de5c6a9a71e
GabrielaVegaPuga <69325261+gabrielavegapuga@users.noreply.github.com>
2022-03-18 11:37:49 -0300

    OT166-53: model and repository for Contacts (#11)

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/ContactEntity.java
@@ -0,0 +1,43 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "CONTACTS")
+public class ContactEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "CONTACT_ID")
+  private Long id;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @Column(name = "PHONE")
+  private String phone;
+
+  @Column(name = "EMAIL", nullable = false)
+  private String email;
+
+  @Column(name = "MESSAGE", nullable = false)
+  private String message;
+
+  @Column(name = "DELETED_AT", nullable = false)
+  private Date deletedAt;
+
+}
