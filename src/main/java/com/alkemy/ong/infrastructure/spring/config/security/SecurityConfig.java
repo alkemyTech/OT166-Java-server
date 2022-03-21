36c07a27226c25d093278006253fdf27fe65bacc
Kayki <jair.garcia.montano@gmail.com>
2022-03-21 15:47:48 -0300

    OT166-33: AuthenticationManager added

--- src/main/java/com/alkemy/ong/infrastructure/spring/config/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/spring/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,6 +22,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests().anyRequest().permitAll();
     /*http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);*/
+  }
+  
+  @Override 
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
   }
 
 }
