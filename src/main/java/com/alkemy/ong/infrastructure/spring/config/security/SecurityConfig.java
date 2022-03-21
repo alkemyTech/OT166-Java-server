868e77fc8655e92f05f93272d37068b2d7a8dcba
Kayki <jair.garcia.montano@gmail.com>
2022-03-21 15:54:17 -0300

    OT166-33: AuthenticationEntryPoint added

--- src/main/java/com/alkemy/ong/infrastructure/spring/config/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/spring/config/security/SecurityConfig.java
@@ -3,12 +3,15 @@ package com.alkemy.ong.infrastructure.spring.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @EnableWebSecurity
 @Configuration
@@ -20,13 +23,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().permitAll();
-    /*http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);*/
+    http.authorizeRequests().anyRequest().permitAll().and().httpBasic()
+        .authenticationEntryPoint(authenticationEntryPoint());
+    /* http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class); */
   }
-  
-  @Override 
+
+  @Override
   public AuthenticationManager authenticationManagerBean() throws Exception {
     return super.authenticationManagerBean();
+  }
+
+  @Bean
+  public AuthenticationEntryPoint authenticationEntryPoint() {
+    BasicAuthenticationEntryPoint entryPoint = new BasicAuthenticationEntryPoint();
+    entryPoint.setRealmName("admin realm");
+    return entryPoint;
   }
 
 }
