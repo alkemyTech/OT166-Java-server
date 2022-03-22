9fae3f50a5c55a9e9d7b6ae500ce5fb16c912c38
Jair Kayki Garcia Montaño <56768010+KaykiPrex@users.noreply.github.com>
2022-03-22 06:20:15 -0300

    OT166-33: validate role (#18)

--- src/main/java/com/alkemy/ong/infrastructure/spring/config/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/spring/config/security/SecurityConfig.java
@@ -3,10 +3,13 @@ package com.alkemy.ong.infrastructure.spring.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -21,6 +24,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests().anyRequest().permitAll();
     /*http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);*/
+    http.exceptionHandling().authenticationEntryPoint(forbiddenEntryPoint());
+  }
+
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Bean
+  public Http403ForbiddenEntryPoint forbiddenEntryPoint() {
+    return new Http403ForbiddenEntryPoint();
+
   }
 
 }
