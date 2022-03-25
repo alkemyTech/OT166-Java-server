c5cd09cd7fe054b1dfe66ea004d7a5016fd60a3d
Facundo Villegas <facucba@gmail.com>
2022-03-25 02:09:21 -0300

    CategoryResponse created

---
+++ src/main/java/com/alkemy/ong/application/rest/response/CategoryResponse.java
@@ -0,0 +1,22 @@
+package com.alkemy.ong.application.rest.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryResponse {
+
+  private Long id;
+
+  private String name;
+
+  private String description;
+
+  private String image;
+
+}
