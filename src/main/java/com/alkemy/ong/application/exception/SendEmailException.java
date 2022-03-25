9aa8aeb28848c9762bfefa25cff00fbb66f5dee4
Matias Fiorentini <mati.fiore1996@gmail.com>
2022-03-25 16:53:49 -0300

    OT166-27: bug fixed clase Exception

--- src/main/java/com/alkemy/ong/application/exception/SendEmailException.java
+++ src/main/java/com/alkemy/ong/application/exception/SendEmailException.java
@@ -1,8 +1,8 @@
 package com.alkemy.ong.application.exception;
 
-public class SendEmailException extends RuntimeException{
+public class SendEmailException extends RuntimeException {
 
-  public SendEmailException(String message){
+  public SendEmailException(String message) {
     super(message);
   }
 
