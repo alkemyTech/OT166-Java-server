11262f786efc5e8b90699f4d511ce31082709ebc
Facundo Villegas <facucba@gmail.com>
2022-03-26 00:43:00 -0300

    OT166-42: Unify CategoryService methods

--- src/main/java/com/alkemy/ong/application/service/CategoryService.java
+++ src/main/java/com/alkemy/ong/application/service/CategoryService.java
@@ -1,16 +1,19 @@
 package com.alkemy.ong.application.service;
 
+import com.alkemy.ong.application.exception.EntityNotFoundException;
 import com.alkemy.ong.application.rest.request.CategoryRequest;
 import com.alkemy.ong.application.rest.response.CategoryResponse;
 import com.alkemy.ong.application.service.abstraction.ICreateCategoryService;
+import com.alkemy.ong.application.service.abstraction.IDeleteCategoryService;
 import com.alkemy.ong.infrastructure.database.entity.CategoryEntity;
 import com.alkemy.ong.infrastructure.database.mapper.abstraction.ICategoryMapper;
 import com.alkemy.ong.infrastructure.database.repository.ICategoryRepository;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CategoryService implements ICreateCategoryService {
+public class CategoryService implements ICreateCategoryService, IDeleteCategoryService {
 
   @Autowired
   private ICategoryRepository categoryRepository;
@@ -24,5 +27,17 @@ public class CategoryService implements ICreateCategoryService {
     entity.setSoftDeleted(false);
     categoryRepository.save(entity);
     return categoryMapper.toCategoryResponse(entity);
+  }
+
+  @Override
+  public void delete(Long id) {
+    Optional<CategoryEntity> result = categoryRepository.findById(id);
+    if (result.isEmpty() || Boolean.TRUE.equals(result.get().getSoftDeleted())) {
+      throw new EntityNotFoundException("Category not found.");
+    }
+
+    CategoryEntity categoryEntity = result.get();
+    categoryEntity.setSoftDeleted(true);
+    categoryRepository.save(categoryEntity);
   }
 }
