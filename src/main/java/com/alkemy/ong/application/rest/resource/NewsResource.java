1e39f6eddfd6890e40d74128c9ca03a7c629f05e
Jair Kayki Garcia Montaño <56768010+KaykiPrex@users.noreply.github.com>
2022-03-27 14:20:53 -0300

    OT166-46: create news endpoint (#23)

---
+++ src/main/java/com/alkemy/ong/application/rest/resource/NewsResource.java
@@ -0,0 +1,39 @@
+package com.alkemy.ong.application.rest.resource;
+
+import com.alkemy.ong.application.rest.request.CreateNewsRequest;
+import com.alkemy.ong.application.rest.response.NewsResponse;
+import com.alkemy.ong.application.service.abstraction.ICreateNewsService;
+import java.net.URI;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("news")
+public class NewsResource {
+
+  @Autowired
+  private ICreateNewsService createNewsService;
+
+  @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+      consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<NewsResponse> postNews(
+      @Valid @RequestBody CreateNewsRequest createNewsRequest) {
+
+    NewsResponse newsResponse = createNewsService.create(createNewsRequest);
+    URI location = ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(newsResponse.getId())
+        .toUri();
+
+    return ResponseEntity.created(location).body(newsResponse);
+  }
+
+}
--- src/main/java/com/alkemy/ong/infrastructure/database/repository/ICategoryRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/ICategoryRepository.java
@@ -1,10 +1,13 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.infrastructure.database.entity.CategoryEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICategoryRepository extends JpaRepository<CategoryEntity, Long> {
+  
+  Optional<CategoryEntity>  findByName(String name);
 
 }
