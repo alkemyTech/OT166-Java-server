0ad3394a69179711bb8a5009e442e4a0f3fab222
Matias <unity.matias@outlook.com>
2022-03-27 21:04:09 -0300

    regular expressions admit characters from the Spanish language

--- src/main/java/com/alkemy/ong/application/util/RegExpressionUtils.java
+++ src/main/java/com/alkemy/ong/application/util/RegExpressionUtils.java
@@ -2,9 +2,9 @@ package com.alkemy.ong.application.util;
 
 public class RegExpressionUtils {
 
-  public static final String ALPHABETIC_CHARACTERS_WITH_BLANK_SPACES = "^[a-zA-Z_ ]*$";
-  public static final String ALPHANUMERIC_CHARACTERS_WITHOUT_BLANK_SPACES = "^[a-zA-Z0-9_]*$";
-  public static final String ALPHANUMERIC_CHARACTERS_WITH_BLANK_SPACES = "^[a-zA-Z0-9_ ]*$";
+  public static final String ALPHABETIC_CHARACTERS_WITH_BLANK_SPACES = "^[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ_ ]*$";
+  public static final String ALPHANUMERIC_CHARACTERS_WITHOUT_BLANK_SPACES = "^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚüÜ_]*$";
+  public static final String ALPHANUMERIC_CHARACTERS_WITH_BLANK_SPACES = "^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚüÜ_ ]*$";
 
   private RegExpressionUtils() {
 
