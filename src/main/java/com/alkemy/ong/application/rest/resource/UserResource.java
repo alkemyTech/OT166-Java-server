0b7a00a98def5fcabf748f130af92a9cf2c684f4
adrianlunau <adrianelunau@gmail.com>
2022-03-29 19:01:59 -0300

    OT166-35: Agregue metodo get a UserResource.

--- src/main/java/com/alkemy/ong/application/rest/resource/UserResource.java
+++ src/main/java/com/alkemy/ong/application/rest/resource/UserResource.java
@@ -1,10 +1,14 @@
 package com.alkemy.ong.application.rest.resource;
 
+import com.alkemy.ong.application.rest.response.UserResponse;
 import com.alkemy.ong.application.service.abstraction.IDeleteUserService;
+import com.alkemy.ong.application.service.abstraction.IGetListUserService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +19,18 @@ public class UserResource {
 
   @Autowired
   private IDeleteUserService deleteUserService;
+  @Autowired
+  private IGetListUserService getListUserService;
 
   @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     deleteUserService.delete(id);
     return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<UserResponse>> listUserDetails() {
+    return ResponseEntity.ok().body(getListUserService.getList());
   }
 
 }
