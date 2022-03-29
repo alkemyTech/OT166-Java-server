9140f4fa71f5abce618ce95a3124d07f789e7087
adrianlunau <adrianelunau@gmail.com>
2022-03-29 18:45:26 -0300

    OT166-35: Agregue metodos en iUserMapper

--- src/main/java/com/alkemy/ong/infrastructure/database/mapper/abstraction/IUserMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/mapper/abstraction/IUserMapper.java
@@ -2,7 +2,9 @@ package com.alkemy.ong.infrastructure.database.mapper.abstraction;
 
 import com.alkemy.ong.application.rest.request.RegisterRequest;
 import com.alkemy.ong.application.rest.response.RegisterResponse;
+import com.alkemy.ong.application.rest.response.UserResponse;
 import com.alkemy.ong.infrastructure.database.entity.UserEntity;
+import java.util.List;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -11,5 +13,9 @@ public interface IUserMapper {
   UserEntity toUserEntity(RegisterRequest registerRequest);
 
   RegisterResponse toRegisterResponse(UserEntity userEntity);
+
+  UserResponse toUserResponse(UserEntity userEntity);
+
+  List<UserResponse> toListUserResponse(List<UserEntity> entities);
 
 }
