bcc4aa002c106e824471da6640a6db2f680468db
Matias Fiorentini <mati.fiore1996@gmail.com>
2022-03-30 15:51:41 -0300

    se agregó al big test en metodo saveOrganization los campos de redes sociales

--- src/test/java/com/alkemy/ong/bigtest/util/BigTest.java
+++ src/test/java/com/alkemy/ong/bigtest/util/BigTest.java
@@ -108,6 +108,9 @@ public abstract class BigTest {
         .email("somos.mas@ong.com")
         .phone("+5411345678")
         .address("Elm Street 3")
+        .facebookUrl("Somos_Mas")
+        .linkedInUrl("Somos Mas")
+        .instagramUrl("SomosMas")
         .build());
   }
 
