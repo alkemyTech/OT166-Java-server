6e047cbcef5cbb98e7db8a33edf135eee60bff45
AlfredRdz <alfredo12_1998@hotmail.com>
2022-03-30 14:28:34 -0600

    OT166-54: fix

--- src/main/java/com/alkemy/ong/application/rest/response/ContactResponse.java
+++ src/main/java/com/alkemy/ong/application/rest/response/ContactResponse.java
@@ -1,6 +1,5 @@
 package com.alkemy.ong.application.rest.response;
 
-import com.amazonaws.services.dynamodbv2.xspec.S;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
