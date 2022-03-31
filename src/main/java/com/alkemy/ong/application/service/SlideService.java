2bbaf742db83f93f908ac61cb65e5ae9b967e926
Kayki <jair.garcia.montano@gmail.com>
2022-03-31 19:15:55 -0300

    OT166-60: IGetSlideService added with impl and mapper

--- src/main/java/com/alkemy/ong/application/service/SlideService.java
+++ src/main/java/com/alkemy/ong/application/service/SlideService.java
@@ -1,16 +1,24 @@
 package com.alkemy.ong.application.service;
 
 import com.alkemy.ong.application.exception.EntityNotFoundException;
+import com.alkemy.ong.application.rest.response.SlideResponse;
 import com.alkemy.ong.application.service.abstraction.IDeleteSlideService;
+import com.alkemy.ong.application.service.abstraction.IGetSlideService;
+import com.alkemy.ong.infrastructure.database.entity.SlideEntity;
+import com.alkemy.ong.infrastructure.database.mapper.abstraction.ISlideMapper;
 import com.alkemy.ong.infrastructure.database.repository.ISlideRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SlideService implements IDeleteSlideService {
+public class SlideService implements IDeleteSlideService, IGetSlideService {
 
   @Autowired
   private ISlideRepository slideRepository;
+  
+  @Autowired
+  private ISlideMapper slideMapper;
 
   @Override
   public void delete(Long id) {
@@ -18,5 +26,15 @@ public class SlideService implements IDeleteSlideService {
       throw new EntityNotFoundException("Slide not found.");
     }
     slideRepository.deleteById(id);
+  }
+
+  @Override
+  public List<SlideResponse> listSlides() {
+    List<SlideEntity> slideEntities = slideRepository.findAllByOrderByOrder();
+    if (slideEntities.isEmpty()) {
+      throw new EntityNotFoundException("Missing record in slide table.");
+    }
+    
+    return slideMapper.toListSlideResponse(slideEntities);
   }
 }
