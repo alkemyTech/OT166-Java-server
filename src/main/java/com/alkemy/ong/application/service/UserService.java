c141d793bc457f6a81b382621d10ba873dd74ae7
Facundo Villegas <facucba@gmail.com>
2022-03-31 14:47:28 -0300

    OT166-34: Update method and patch logic added to UserService.

--- src/main/java/com/alkemy/ong/application/service/UserService.java
+++ src/main/java/com/alkemy/ong/application/service/UserService.java
@@ -1,8 +1,14 @@
 package com.alkemy.ong.application.service;
 
+import static com.alkemy.ong.application.util.RegExpressionUtils.ALPHABETIC_CHARACTERS_WITH_BLANK_SPACES;
+
 import com.alkemy.ong.application.exception.EntityNotFoundException;
+import com.alkemy.ong.application.rest.request.UpdateUserRequest;
+import com.alkemy.ong.application.rest.response.UpdatedUserResponse;
 import com.alkemy.ong.application.service.abstraction.IDeleteUserService;
+import com.alkemy.ong.application.service.abstraction.IUpdateUserService;
 import com.alkemy.ong.infrastructure.database.entity.UserEntity;
+import com.alkemy.ong.infrastructure.database.mapper.abstraction.IUserMapper;
 import com.alkemy.ong.infrastructure.database.repository.IUserRepository;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
@@ -10,16 +16,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @AllArgsConstructor
-public class UserService implements UserDetailsService, IDeleteUserService {
+public class UserService implements UserDetailsService, IDeleteUserService, IUpdateUserService {
 
   @Autowired
   private IUserRepository userRepository;
 
+  @Autowired
+  private IUserMapper userMapper;
 
   @Override
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -39,6 +48,26 @@ public class UserService implements UserDetailsService, IDeleteUserService {
     UserEntity userEntity = findBy(id);
     userEntity.setSoftDeleted(true);
     userRepository.save(userEntity);
+  }
+
+  @Override
+  public UpdatedUserResponse update(Long id, UpdateUserRequest updateUserRequest) {
+    UserEntity userEntity = findBy(id);
+    String firstName = updateUserRequest.getFirstName();
+    String lastName = updateUserRequest.getLastName();
+    String password = updateUserRequest.getPassword();
+
+    if (firstName != null) {
+      userEntity.setFirstName(updateUserRequest.getFirstName());
+    }
+    if (lastName != null) {
+      userEntity.setLastName(updateUserRequest.getLastName());
+    }
+    if (password != null) {
+      userEntity.setPassword(new BCryptPasswordEncoder().encode(updateUserRequest.getPassword()));
+    }
+
+    return userMapper.toUpdatedUserResponse(userRepository.save(userEntity));
   }
 
   private UserEntity findBy(Long id) {
