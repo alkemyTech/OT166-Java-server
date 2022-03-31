b7b664089a3f57829f1405fa3d40d4b7f6972993
Kayki <jair.garcia.montano@gmail.com>
2022-03-31 19:22:08 -0300

    OT166-60: Organization response added

--- src/main/java/com/alkemy/ong/application/rest/response/OrganizationResponse.java
+++ src/main/java/com/alkemy/ong/application/rest/response/OrganizationResponse.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.application.rest.response;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,5 +17,6 @@ public class OrganizationResponse {
   private String image;
   private String address;
   private String phone;
+  private List<SlideResponse> slides;
 
 }
