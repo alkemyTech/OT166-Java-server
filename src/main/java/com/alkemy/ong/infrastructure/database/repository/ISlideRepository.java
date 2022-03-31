8d76aa41ee8ff4a64207941499d9d29b618441c7
Kayki <jair.garcia.montano@gmail.com>
2022-03-31 19:10:59 -0300

    OT166-60: List of Slide by order in Slide repository added

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/ISlideRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/ISlideRepository.java
@@ -1,10 +1,13 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.infrastructure.database.entity.SlideEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ISlideRepository extends JpaRepository<SlideEntity, Long> {
+  
+  List<SlideEntity> findAllByOrderByOrder();
 
 }
