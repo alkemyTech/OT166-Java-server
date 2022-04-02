0be0b0525531544621f69be321854a4bc200a398
Kayki <jair.garcia.montano@gmail.com>
2022-04-02 17:14:57 -0300

    OT166-36: extractUsernameFromHeader is added in JwtUtils

--- src/main/java/com/alkemy/ong/infrastructure/util/JwtUtils.java
+++ src/main/java/com/alkemy/ong/infrastructure/util/JwtUtils.java
@@ -28,6 +28,10 @@ public class JwtUtils {
     return extractClaim(token, Claims::getSubject);
   }
 
+  public String extractUsernameFromHeader(String authorizationHeader) {
+    return extractUsername(getToken(authorizationHeader));
+  }
+
   private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
     final Claims claims = extractAllClaims(token);
     return claimsResolver.apply(claims);
