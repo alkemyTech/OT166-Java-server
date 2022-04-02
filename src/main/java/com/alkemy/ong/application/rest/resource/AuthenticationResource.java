cccbd24e0796c5703c197052737bf0245b7529f8
Kayki <jair.garcia.montano@gmail.com>
2022-04-02 17:20:54 -0300

    OT166-36: Endpoint /auth/me added

--- src/main/java/com/alkemy/ong/application/rest/resource/AuthenticationResource.java
+++ src/main/java/com/alkemy/ong/application/rest/resource/AuthenticationResource.java
@@ -4,13 +4,17 @@ import com.alkemy.ong.application.rest.request.AuthenticationRequest;
 import com.alkemy.ong.application.rest.request.RegisterRequest;
 import com.alkemy.ong.application.rest.response.AuthenticationResponse;
 import com.alkemy.ong.application.rest.response.RegisterResponse;
+import com.alkemy.ong.application.rest.response.UserResponse;
 import com.alkemy.ong.application.service.abstraction.IAuthenticationService;
+import com.alkemy.ong.application.service.abstraction.IGetUserAuthenticateService;
 import com.alkemy.ong.application.service.abstraction.IRegisterService;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +29,9 @@ public class AuthenticationResource {
 
   @Autowired
   private IRegisterService registerService;
+  
+  @Autowired
+  private IGetUserAuthenticateService userService;
 
   @PostMapping(path = "/register",
       consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -42,6 +49,11 @@ public class AuthenticationResource {
       @Valid @RequestBody AuthenticationRequest authenticationRequest) {
     return ResponseEntity.ok().body(authService.login(authenticationRequest));
   }
-
+  
+  @GetMapping(path = "/me", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<UserResponse> getUser(HttpServletRequest httpServletRequest) {
+    return ResponseEntity.ok().body(userService.getUserAuthenticated(
+        httpServletRequest.getHeader("Authorization")));
+  }
 }
 
