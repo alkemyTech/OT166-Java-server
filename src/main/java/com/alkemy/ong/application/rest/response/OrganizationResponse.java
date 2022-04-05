5c2c6d4c183918f784585fd7a68b9ce770b39dd1
Matias Fiorentini <mati.fiore1996@gmail.com>
2022-04-05 09:29:13 -0300

    modificación por annotation en OrganizationResponse

--- src/main/java/com/alkemy/ong/application/rest/response/OrganizationResponse.java
+++ src/main/java/com/alkemy/ong/application/rest/response/OrganizationResponse.java
@@ -1,7 +1,6 @@
 package com.alkemy.ong.application.rest.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +12,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrganizationResponse {
 
   private String name;
@@ -20,15 +20,9 @@ public class OrganizationResponse {
   private String address;
   private String phone;
   private List<SlideResponse> slides;
-  private SocialMedia socialMedia;
-
-  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private SocialMediaResponse socialMedia;
   private String email;
-
-  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String welcomeText;
-
-  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String aboutUsText;
 
 }
