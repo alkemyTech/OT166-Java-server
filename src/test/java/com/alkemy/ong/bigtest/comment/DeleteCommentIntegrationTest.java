e979f4b3fac6a4e2ffddb56caefe67edab7f8edb
Magali Kain <magali.kain@thoughtworks.com>
2022-04-06 23:55:28 +0200

    Add extra asserts to delete integration test since those are use as references.

--- src/test/java/com/alkemy/ong/bigtest/comment/DeleteCommentIntegrationTest.java
+++ src/test/java/com/alkemy/ong/bigtest/comment/DeleteCommentIntegrationTest.java
@@ -1,9 +1,12 @@
 package com.alkemy.ong.bigtest.comment;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.alkemy.ong.bigtest.util.BigTest;
+import com.alkemy.ong.infrastructure.database.entity.CommentEntity;
+import java.util.Optional;
 import org.junit.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -18,6 +21,14 @@ public class DeleteCommentIntegrationTest extends BigTest {
             .contentType(MediaType.APPLICATION_JSON)
             .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser()))
         .andExpect(status().isNoContent());
+
+    assertCommentHasBeenDeleted(id);
+  }
+
+
+  private void assertCommentHasBeenDeleted(Long id) {
+    Optional<CommentEntity> optionalCommentEntity = commentRepository.findById(id);
+    assertTrue(optionalCommentEntity.isEmpty());
   }
 
 }
--- src/test/java/com/alkemy/ong/bigtest/slide/DeleteSlideIntegrationTest.java
+++ src/test/java/com/alkemy/ong/bigtest/slide/DeleteSlideIntegrationTest.java
@@ -8,7 +8,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.alkemy.ong.bigtest.util.BigTest;
+import com.alkemy.ong.infrastructure.database.entity.SlideEntity;
+import java.util.Optional;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
@@ -22,6 +25,8 @@ public class DeleteSlideIntegrationTest extends BigTest {
             .contentType(MediaType.APPLICATION_JSON)
             .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser()))
         .andExpect(status().isNoContent());
+
+    assertSlideHasBeenDeleted(id);
   }
 
   @Test
@@ -45,6 +50,11 @@ public class DeleteSlideIntegrationTest extends BigTest {
         .andExpect(jsonPath("$.moreInfo", hasSize(1)))
         .andExpect(jsonPath("$.moreInfo", hasItem("Slide not found.")))
         .andExpect(status().isNotFound());
+  }
+
+  private void assertSlideHasBeenDeleted(Long id) {
+    Optional<SlideEntity> optionalSlideEntity = slideRepository.findById(id);
+    Assertions.assertTrue(optionalSlideEntity.isEmpty());
   }
 
 }
