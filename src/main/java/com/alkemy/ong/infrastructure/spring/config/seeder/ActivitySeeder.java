afa711edb5bd098b0fca39145fd8535d2bc23e44
Facundo Villegas <facucba@gmail.com>
2022-04-07 18:38:25 -0300

    OT166-52: Fixed all commented code.

--- src/main/java/com/alkemy/ong/infrastructure/spring/config/seeder/ActivitySeeder.java
+++ src/main/java/com/alkemy/ong/infrastructure/spring/config/seeder/ActivitySeeder.java
@@ -13,61 +13,39 @@ import org.springframework.stereotype.Component;
 public class ActivitySeeder {
 
   private static final List<String> ACTIVITIES_NAMES = List.of(
-      "Apoyo Escolar para el nivel Primario", "Apoyo Escolar Nivel Secundaria", "Tutorías");
-  private static final List<String> ACTIVITIES_CONTENTS = List.of("El espacio de apoyo escolar es "
-      + "el corazón del área educativa. Se realizan los "
-      + "talleres de lunes a jueves de 10 a 12 horas y de 14 a 16 horas en el "
-      + "contraturno, Los sábados también se realiza el taller para niños y niñas que "
-      + "asisten a la escuela doble turno. Tenemos un espacio especial para los de "
-      + "1er grado 2 veces por semana ya que ellos necesitan atención especial! "
-      + "Actualmente se encuentran inscriptos a este programa 150 niños y niñas de "
-      + "6 a 15 años. Este taller está pensado para ayudar a los alumnos con el "
-      + "material que traen de la escuela, también tenemos una docente que les da "
-      + "clases de lengua y matemática con una planificación propia que armamos "
-      + "en Manos para nivelar a los niños y que vayan con más herramientas a la "
-      + "escuela.", "Del mismo modo que en primaria, este taller es el corazón del área "
-      + "secundaria. Se realizan talleres de lunes a viernes de 10 a 12 horas y de 16 a "
-      + "18 horas en el contraturno. Actualmente se encuentran inscriptos en el taller "
-      + "50 adolescentes entre 13 y 20 años. Aquí los jóvenes se presentan con el "
-      + "material que traen del colegio y una docente de la institución y un grupo de "
-      + "voluntarios los recibe para ayudarlos a estudiar o hacer la tarea. Este "
-      + "espacio también es utilizado por los jóvenes como un punto de encuentro y "
-      + "relación entre ellos y la institución.", "Es un programa destinado a jóvenes a "
-      + "partir del tercer año de secundaria, "
-      + "cuyo objetivo es garantizar su permanencia en la escuela y construir un "
-      + "proyecto de vida que da sentido al colegio. El objetivo de esta propuesta es "
-      + "lograr la integración escolar de niños y jóvenes del barrio promoviendo el "
-      + "soporte socioeducativo y emocional apropiado, desarrollando los recursos "
-      + "institucionales necesarios a través de la articulación de nuestras "
-      + "intervenciones con las escuelas que los alojan, con las familias de los "
-      + "alumnos y con las instancias municipales, provinciales y nacionales que "
-      + "correspondan.");
-  private static final String ACTIVITY_IMAGE = "url/de/la/imagen";
+      "Elementary School school-based support programmes",
+      "High School school-based support progammes", "Lessons");
+  private static final List<String> ACTIVITIES_CONTENTS = List.of("The school support space is the "
+      + "heart of the educational area.This workshop is designed to help students with the "
+      + "material they bring from school, we also have a teacher who gives them "
+      + "language and math classes.","Here the young people present themselves with the "
+      + "material they bring from school and a teacher from the institution and a group of "
+      + "volunteers welcomes them to help them study or do homework.", "It is a program aimed at "
+      + "young people from the third year of high school, "
+      + "whose objective is to guarantee their permanence in school and build a "
+      + "life project that gives meaning to the school.");
+  private static final String ACTIVITY_IMAGE = "https://aws.amazon.com/activity/activity-image.img";
 
   @Autowired
   private IActivityRepository activityRepository;
 
   @EventListener
   public void seed(ContextRefreshedEvent event) {
-    createActivityTable();
+    createActivities();
   }
 
-  private void createActivityTable() {
-    createActivity();
-  }
-
-  private void createActivity() {
+  private void createActivities() {
     if (activityRepository.count() < 3) {
-      saveActivities(ACTIVITIES_NAMES, ACTIVITIES_CONTENTS);
+      saveActivities();
     }
   }
 
-  private void saveActivities(List<String> names, List<String> contents) {
+  private void saveActivities() {
     List<ActivityEntity> activities = new ArrayList<>(3);
-    for (int i = 0; i < 3; i++) {
+    for (int index = 0; index < 3; index++) {
       activities.add(buildActivity(
-          names.get(i),
-          contents.get(i)));
+          ACTIVITIES_NAMES.get(index),
+          ACTIVITIES_CONTENTS.get(index)));
     }
     activityRepository.saveAll(activities);
   }
