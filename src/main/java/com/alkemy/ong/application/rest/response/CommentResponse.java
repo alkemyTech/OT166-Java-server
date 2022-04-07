f21a456672d5417024f3f2543fb018c03a5fb586
Matias Fiorentini <mati.fiore1996@gmail.com>
2022-04-07 20:09:31 -0300

    agrego nuevos campos

--- src/main/java/com/alkemy/ong/application/rest/response/CommentResponse.java
+++ src/main/java/com/alkemy/ong/application/rest/response/CommentResponse.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.application.rest.response;
 
+import java.sql.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,8 @@ public class CommentResponse {
 
   private Long id;
   private String body;
+  private String createdBy;
+  private String associatedNews;
+  private Timestamp createTimestamp;
 
 }
