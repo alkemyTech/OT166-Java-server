1d74df5fb119cbc553a631ffccd469efd49df2ea
Matias Fiorentini <mati.fiore1996@gmail.com>
2022-04-07 14:52:45 -0300

    creación commentResponse

---
+++ src/main/java/com/alkemy/ong/application/rest/response/CommentResponse.java
@@ -0,0 +1,18 @@
+package com.alkemy.ong.application.rest.response;
+
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CommentResponse {
+
+  private Long id;
+  private String body;
+
+}
