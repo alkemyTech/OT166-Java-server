6e2e3594d2339e1e0e876ec209c2ff2df83492be
adrianlunau <adrianelunau@gmail.com>
2022-04-07 16:58:26 -0300

    OT166-71: Agregue UpdateMemberRequest

---
+++ src/main/java/com/alkemy/ong/application/rest/request/UpdateMemberRequest.java
@@ -0,0 +1,27 @@
+package com.alkemy.ong.application.rest.request;
+
+import static com.alkemy.ong.application.util.RegExpressionUtils.ALPHABETIC_CHARACTERS_WITH_BLANK_SPACES;
+import static com.alkemy.ong.application.util.RegExpressionUtils.URL;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class UpdateMemberRequest {
+
+  @Pattern(regexp = ALPHABETIC_CHARACTERS_WITH_BLANK_SPACES,
+      message = "The name has invalid format.")
+  private String name;
+  private String facebookUrl;
+  private String instagramUrl;
+  private String linkedInUrl;
+  @Pattern(regexp = URL, message = "The image has invalid format.")
+  private String image;
+  private String description;
+
+}
