9cf9b34f6113f4856b0429c3692c0c4019a22c8e
celesdv <66212987+celesdv@users.noreply.github.com>
2022-04-08 15:52:44 -0300

    OT166-66 name NotNull

--- src/main/java/com/alkemy/ong/application/rest/request/UpdateTestimonialRequest.java
+++ src/main/java/com/alkemy/ong/application/rest/request/UpdateTestimonialRequest.java
@@ -1,8 +1,10 @@
 package com.alkemy.ong.application.rest.request;
 
 import com.alkemy.ong.application.util.RegExpressionUtils;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +14,7 @@ import lombok.Setter;
 @Builder
 public class UpdateTestimonialRequest {
 
+  @NotNull(message = "The name must not be null")
   @Size(max = 50, message = "The name attribute must not be more than 50 characters")
   @Pattern(regexp = RegExpressionUtils.ALPHABETIC_CHARACTERS_WITH_BLANK_SPACES,
       message = "The name has invalid format.")
