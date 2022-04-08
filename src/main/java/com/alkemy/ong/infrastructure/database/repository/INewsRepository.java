a5ccdf27e661c14d2db58e3d21a76fce2a1cdff1
Matias Fiorentini <mati.fiore1996@gmail.com>
2022-04-08 20:14:29 -0300

    OT166-81: agrego query en INewsRepository

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/INewsRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/INewsRepository.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.infrastructure.database.entity.NewsEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface INewsRepository extends JpaRepository<NewsEntity, Long> {
 
   NewsEntity findByIdAndSoftDeletedFalse(Long id);
+
+  Page<NewsEntity> findBySoftDeletedFalseOrderByIdAsc(Pageable pageable);
 
 }
