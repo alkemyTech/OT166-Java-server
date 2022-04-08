df33af916607ce0ec5cc42a79cde91533255ed3d
Jair Kayki Garcia Montaño <56768010+KaykiPrex@users.noreply.github.com>
2022-04-08 05:20:20 -0300

    OT166-40: list categories endpoint (#66)

--- src/main/java/com/alkemy/ong/application/service/CategoryService.java
+++ src/main/java/com/alkemy/ong/application/service/CategoryService.java
@@ -4,6 +4,7 @@ import com.alkemy.ong.application.exception.EntityNotFoundException;
 import com.alkemy.ong.application.rest.request.CreateCategoryRequest;
 import com.alkemy.ong.application.rest.request.UpdateCategoryRequest;
 import com.alkemy.ong.application.rest.response.CategoryResponse;
+import com.alkemy.ong.application.rest.response.ListCategoriesResponse;
 import com.alkemy.ong.application.service.abstraction.ICreateCategoryService;
 import com.alkemy.ong.application.service.abstraction.IDeleteCategoryService;
 import com.alkemy.ong.application.service.abstraction.IGetCategoryService;
@@ -11,6 +12,7 @@ import com.alkemy.ong.application.service.abstraction.IUpdateCategoryService;
 import com.alkemy.ong.infrastructure.database.entity.CategoryEntity;
 import com.alkemy.ong.infrastructure.database.mapper.abstraction.ICategoryMapper;
 import com.alkemy.ong.infrastructure.database.repository.ICategoryRepository;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -45,6 +47,14 @@ public class CategoryService implements ICreateCategoryService, IDeleteCategoryS
     return categoryMapper.toCategoryResponse(findBy(id));
   }
 
+  @Override
+  public ListCategoriesResponse listActiveCategories() {
+    List<CategoryEntity> categoryEntities = categoryRepository.findBySoftDeletedIsFalse();
+    ListCategoriesResponse listCategoriesResponse = new ListCategoriesResponse();
+    listCategoriesResponse.setCategories(categoryMapper.toCategoryNameResponses(categoryEntities));
+    return listCategoriesResponse;
+  }
+
   private CategoryEntity findBy(Long id) {
     Optional<CategoryEntity> optionalCategoryEntity = categoryRepository.findById(id);
     if (optionalCategoryEntity.isEmpty()
@@ -53,7 +63,6 @@ public class CategoryService implements ICreateCategoryService, IDeleteCategoryS
     }
     return optionalCategoryEntity.get();
   }
-
 
   @Override
   public CategoryResponse update(Long id, UpdateCategoryRequest updateCategoryRequest) {
--- src/main/java/com/alkemy/ong/infrastructure/database/mapper/abstraction/ICategoryMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/mapper/abstraction/ICategoryMapper.java
@@ -3,7 +3,10 @@ package com.alkemy.ong.infrastructure.database.mapper.abstraction;
 import com.alkemy.ong.application.rest.request.CreateCategoryRequest;
 import com.alkemy.ong.application.rest.response.CategoryResponse;
 import com.alkemy.ong.infrastructure.database.entity.CategoryEntity;
+import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface ICategoryMapper {
@@ -11,5 +14,12 @@ public interface ICategoryMapper {
   CategoryEntity toCategoryEntity(CreateCategoryRequest createCategoryRequest);
 
   CategoryResponse toCategoryResponse(CategoryEntity entity);
+
+  @Mappings({
+      @Mapping(target = "id", ignore = true),
+      @Mapping(target = "description", ignore = true),
+      @Mapping(target = "image", ignore = true),
+  })
+  List<CategoryResponse> toCategoryNameResponses(List<CategoryEntity> categoryEntities);
 
 }
