d016c01df49bc6fc9191f2a720a898be41ec63c0
celesdv <66212987+celesdv@users.noreply.github.com>
2022-04-07 22:01:51 -0300

    OT166-66: Endpoint Actualización Testimonios

---
+++ src/main/java/com/alkemy/ong/application/rest/request/UpdateTestimonialRequest.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong.application.rest.request;
+
+import com.alkemy.ong.application.util.RegExpressionUtils;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UpdateTestimonialRequest {
+
+  @Size(max = 50, message = "The name attribute must not be more than 50 characters")
+  @Pattern(regexp = RegExpressionUtils.ALPHABETIC_CHARACTERS_WITH_BLANK_SPACES,
+      message = "The name has invalid format.")
+  String name;
+
+  @Size(max = 150, message = "The name attribute must not be more than 150 characters")
+  @Pattern(regexp = RegExpressionUtils.ALPHABETIC_CHARACTERS_WITH_BLANK_SPACES,
+      message = "The content has invalid format.")
+  String content;
+
+  String image;
+
+}
