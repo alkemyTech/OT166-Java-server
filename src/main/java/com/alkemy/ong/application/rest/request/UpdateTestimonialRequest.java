b14a7db6f3b8de6b4811bb5ad7eec5858d9481a9
celesdv <66212987+celesdv@users.noreply.github.com>
2022-04-08 08:31:59 -0300

    OT166-66: Anotaciones en el updateTestimonialRequest

--- src/main/java/com/alkemy/ong/application/rest/request/UpdateTestimonialRequest.java
+++ src/main/java/com/alkemy/ong/application/rest/request/UpdateTestimonialRequest.java
@@ -3,15 +3,13 @@ package com.alkemy.ong.application.rest.request;
 import com.alkemy.ong.application.util.RegExpressionUtils;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class UpdateTestimonialRequest {
 
   @Size(max = 50, message = "The name attribute must not be more than 50 characters")
