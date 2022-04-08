d47ec4d3296a12fabd6999ff024d2da09edbaa0e
Matias Fiorentini <mati.fiore1996@gmail.com>
2022-04-08 20:16:32 -0300

    OT166-81: agrego método paginateREsultRetrieved

---
+++ src/main/java/com/alkemy/ong/application/util/PaginatedResultsRetrieved.java
@@ -0,0 +1,75 @@
+package com.alkemy.ong.application.util;
+
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class PaginatedResultsRetrieved {
+
+  public void addLinkHeaderOnPagedResourceRetrieval(
+      final UriComponentsBuilder uriBuilder,
+      final HttpServletResponse response,
+      final String path,
+      final int page,
+      final int totalPages,
+      final int pageSize) {
+
+    uriBuilder.path(path);
+
+    final StringBuilder linkHeader = new StringBuilder();
+    if (hasNextPage(page, totalPages)) {
+      final String uriForNextPage = constructNextPageUri(uriBuilder, page, pageSize);
+      linkHeader.append(createLinkHeader(uriForNextPage, "next"));
+    }
+    if (hasPreviousPage(page)) {
+      final String uriForPrevPage = constructPrevPageUri(uriBuilder, page, pageSize);
+      appendCommandIfNecessary(linkHeader);
+      linkHeader.append(createLinkHeader(uriForPrevPage, "prev"));
+    }
+
+    response.addHeader("Link", linkHeader.toString());
+  }
+
+  private String constructNextPageUri(
+      final UriComponentsBuilder uriBuilder,
+      final int page,
+      final int size) {
+    return uriBuilder.replaceQueryParam("page", page + 1)
+        .replaceQueryParam("size", size)
+        .build()
+        .encode()
+        .toUriString();
+  }
+
+  private String constructPrevPageUri(
+      final UriComponentsBuilder uriBuilder,
+      final int page,
+      final int size) {
+    return uriBuilder.replaceQueryParam("page", page - 1)
+        .replaceQueryParam("size", size)
+        .build()
+        .encode()
+        .toUriString();
+  }
+
+  private boolean hasNextPage(final int page, final int totalPages) {
+    return page < totalPages - 1;
+  }
+
+  private boolean hasPreviousPage(final int page) {
+    return page > 0;
+  }
+
+  private void appendCommandIfNecessary(final StringBuilder linkHeader) {
+    if (linkHeader.length() > 0) {
+      linkHeader.append(", ");
+    }
+  }
+
+  private static String createLinkHeader(final String uri, final String rel) {
+    return "<" + uri + ">; rel=\"" + rel + "\"";
+
+  }
+
+}
