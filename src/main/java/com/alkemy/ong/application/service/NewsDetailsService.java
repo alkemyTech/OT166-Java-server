7ad8285d0b4fcb20d94fbe659f80887ce6a41eff
Facundo Villegas <facucba@gmail.com>
2022-04-10 01:43:41 -0300

    OT166-45: Main branch merged and NewsDetailService created.

---
+++ src/main/java/com/alkemy/ong/application/service/NewsDetailsService.java
@@ -0,0 +1,76 @@
+package com.alkemy.ong.application.service;
+
+import com.alkemy.ong.application.exception.EntityNotFoundException;
+import com.alkemy.ong.application.rest.request.CreateNewsRequest;
+import com.alkemy.ong.application.rest.request.UpdateNewsRequest;
+import com.alkemy.ong.application.rest.response.NewsDetailResponse;
+import com.alkemy.ong.application.rest.response.NewsResponse;
+import com.alkemy.ong.application.service.abstraction.ICreateNewsService;
+import com.alkemy.ong.application.service.abstraction.IDeleteNewsService;
+import com.alkemy.ong.application.service.abstraction.IGetNewsDetailsService;
+import com.alkemy.ong.application.service.abstraction.IUpdateNewsService;
+import com.alkemy.ong.infrastructure.database.entity.CategoryEntity;
+import com.alkemy.ong.infrastructure.database.entity.NewsEntity;
+import com.alkemy.ong.infrastructure.database.mapper.abstraction.INewsMapper;
+import com.alkemy.ong.infrastructure.database.repository.ICategoryRepository;
+import com.alkemy.ong.infrastructure.database.repository.INewsRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NewsDetailsService implements ICreateNewsService, IDeleteNewsService,
+    IUpdateNewsService, IGetNewsDetailsService {
+
+  @Autowired
+  private INewsRepository newsRepository;
+  @Autowired
+  private ICategoryRepository categoryRepository;
+  @Autowired
+  private INewsMapper newsMapper;
+
+  @Override
+  public NewsResponse create(CreateNewsRequest newsRequest) {
+    Optional<CategoryEntity> categoryNews = categoryRepository.findByName("news");
+    if (categoryNews.isEmpty()) {
+      throw new EntityNotFoundException("Missing record in category table.");
+    }
+
+    NewsEntity newsEntity = newsMapper.toNewsEntity(newsRequest);
+    newsEntity.setCategory(categoryNews.get());
+    newsEntity.setSoftDeleted(false);
+    return newsMapper.toNewsResponse(newsRepository.save(newsEntity));
+  }
+
+  @Override
+  public NewsResponse update(Long id, UpdateNewsRequest updateNewsRequest) {
+    NewsEntity newsEntity = findBy(id);
+    newsEntity.setName(updateNewsRequest.getName());
+    newsEntity.setContent(updateNewsRequest.getText());
+    newsEntity.setImage(updateNewsRequest.getImage());
+    return newsMapper.toNewsResponse(newsRepository.save(newsEntity));
+  }
+
+  @Override
+  public void delete(Long id) {
+    NewsEntity newsEntity = findBy(id);
+    newsEntity.setSoftDeleted(true);
+    newsRepository.save(newsEntity);
+  }
+
+  private NewsEntity findBy(Long id) {
+    Optional<NewsEntity> optionalNewsEntity = newsRepository.findById(id);
+    if (optionalNewsEntity.isEmpty()
+        || Boolean.TRUE.equals(optionalNewsEntity.get().getSoftDeleted())) {
+      throw new EntityNotFoundException("News not found.");
+    }
+    return optionalNewsEntity.get();
+  }
+
+  @Override
+  public NewsDetailResponse getBy(Long id) {
+    NewsEntity newsEntity = findBy(id);
+    return newsMapper.toNewsDetailResponse(newsEntity);
+  }
+
+}
