20413402b5d968fd1297b80d76a1f6bfb9a536e6
Facundo Villegas <facucba@gmail.com>
2022-04-10 01:59:44 -0300

    OT166-45: getBy method added, IGetNewsDetailService implemented.

--- src/main/java/com/alkemy/ong/application/service/NewsService.java
+++ src/main/java/com/alkemy/ong/application/service/NewsService.java
@@ -19,7 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class NewsDetailService implements ICreateNewsService, IDeleteNewsService,
+public class NewsService implements ICreateNewsService, IDeleteNewsService,
     IUpdateNewsService, IGetNewsDetailService {
 
   @Autowired
