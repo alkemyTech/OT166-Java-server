d35995b65dfb41005650ba940a6dcc263bf7b514
Facundo Villegas <facucba@gmail.com>
2022-04-10 01:54:16 -0300

    OT166-45: getBy method created.

--- src/main/java/com/alkemy/ong/application/rest/resource/NewsResource.java
+++ src/main/java/com/alkemy/ong/application/rest/resource/NewsResource.java
@@ -2,9 +2,11 @@ package com.alkemy.ong.application.rest.resource;
 
 import com.alkemy.ong.application.rest.request.CreateNewsRequest;
 import com.alkemy.ong.application.rest.request.UpdateNewsRequest;
+import com.alkemy.ong.application.rest.response.NewsDetailResponse;
 import com.alkemy.ong.application.rest.response.NewsResponse;
 import com.alkemy.ong.application.service.abstraction.ICreateNewsService;
 import com.alkemy.ong.application.service.abstraction.IDeleteNewsService;
+import com.alkemy.ong.application.service.abstraction.IGetNewsDetailService;
 import com.alkemy.ong.application.service.abstraction.IUpdateNewsService;
 import java.net.URI;
 import javax.validation.Valid;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -32,6 +35,9 @@ public class NewsResource {
 
   @Autowired
   private IDeleteNewsService deleteNewsService;
+
+  @Autowired
+  private IGetNewsDetailService getNewsDetailsService;
 
   @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
       consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -58,6 +64,11 @@ public class NewsResource {
   public ResponseEntity<NewsResponse> update(@PathVariable Long id,
       @Valid @RequestBody UpdateNewsRequest updateNewsRequest) {
     return ResponseEntity.ok(updateNewsService.update(id, updateNewsRequest));
+  }
+
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<NewsDetailResponse> getBy(@PathVariable Long id) {
+    return ResponseEntity.ok().body(getNewsDetailsService.getBy(id));
   }
 
 }
