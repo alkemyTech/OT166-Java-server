f0a003676db3df6ab9355ab2f9433a30576d40d9
Adrian Emiliano Luna Udovicich <85313722+adrianlunau@users.noreply.github.com>
2022-04-12 13:24:48 -0300

    OT166-80: add pagination to list categories endpoint (#78)

--- src/main/java/com/alkemy/ong/application/rest/response/ListCategoriesResponse.java
+++ src/main/java/com/alkemy/ong/application/rest/response/ListCategoriesResponse.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-public class ListCategoriesResponse {
+public class ListCategoriesResponse extends PaginationResponse {
 
   private List<CategoryResponse> categories;
 
--- src/main/java/com/alkemy/ong/infrastructure/database/repository/ICategoryRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/ICategoryRepository.java
@@ -3,6 +3,8 @@ package com.alkemy.ong.infrastructure.database.repository;
 import com.alkemy.ong.infrastructure.database.entity.CategoryEntity;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +13,6 @@ public interface ICategoryRepository extends JpaRepository<CategoryEntity, Long>
 
   Optional<CategoryEntity> findByName(String name);
 
-  List<CategoryEntity> findBySoftDeletedIsFalse();
+  Page<CategoryEntity> findBySoftDeletedIsFalseOrderByIdAsc(Pageable pageable);
 
 }
