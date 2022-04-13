23404f47e1807712d9011ca51ecb0c9188693e34
adrianlunau <adrianelunau@gmail.com>
2022-04-13 16:33:50 -0300

    OT166-77: Agregue endpoint patch comments

---
+++ src/main/java/com/alkemy/ong/application/rest/request/UpdateCommentRequest.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.application.rest.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class UpdateCommentRequest {
+
+  private String body;
+}
