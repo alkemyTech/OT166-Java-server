b2393ee7060e5db832648c29915172305ed25718
Matias Fiorentini <mati.fiore1996@gmail.com>
2022-04-13 19:24:48 -0300

    OT166-88: Add documentation to testimonialResponse

--- src/main/java/com/alkemy/ong/application/rest/response/TestimonialResponse.java
+++ src/main/java/com/alkemy/ong/application/rest/response/TestimonialResponse.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.application.rest.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,10 +9,17 @@ import lombok.Setter;
 @Setter
 public class TestimonialResponse {
 
+  @Schema(example = "25")
   private long id;
+
+  @Schema(example = "Richard Fort")
   private String name;
+
+  @Schema(example = "This is a content")
   private String content;
+
   @JsonInclude(JsonInclude.Include.NON_NULL)
+  @Schema(example = "https://s3.com/richard.fort.jpg")
   private String image;
 
 }
