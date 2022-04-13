767efbe78d7de2855c6dc76b9423a1254e34c83e
Matias Fiorentini <mati.fiore1996@gmail.com>
2022-04-13 19:24:10 -0300

    OT166-88: Add documentation to testimonialResource

--- src/main/java/com/alkemy/ong/application/rest/resource/TestimonialResource.java
+++ src/main/java/com/alkemy/ong/application/rest/resource/TestimonialResource.java
@@ -2,10 +2,17 @@ package com.alkemy.ong.application.rest.resource;
 
 import com.alkemy.ong.application.rest.request.CreateTestimonialRequest;
 import com.alkemy.ong.application.rest.request.UpdateTestimonialRequest;
+import com.alkemy.ong.application.rest.response.ErrorResponse;
 import com.alkemy.ong.application.rest.response.TestimonialResponse;
 import com.alkemy.ong.application.service.abstraction.ICreateTestimonialService;
 import com.alkemy.ong.application.service.abstraction.IDeleteTestimonialService;
 import com.alkemy.ong.application.service.abstraction.IUpdateTestimonialService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@Tag(name = "Testimonial Endpoints", description = "Testimonial Endpoints")
 @RestController
 @RequestMapping("/testimonials")
 public class TestimonialResource {
@@ -33,17 +41,33 @@ public class TestimonialResource {
   @Autowired
   private IUpdateTestimonialService updateTestimonialService;
 
+  @Operation(summary = "Delete Testimonial.", description = "Delete testimonial.", tags = "Delete")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "204", description = "Testimonial deleted."),
+      @ApiResponse(responseCode = "404", description = "Testimonial not found.",
+          content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
   @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     deleteTestimonialService.delete(id);
     return ResponseEntity.noContent().build();
   }
 
+  @Operation(summary = "Create Testimonial.", description = "Create a new testimonial.",
+      tags = "Post")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Returns testimonial created.",
+          content = @Content(schema = @Schema(implementation = TestimonialResponse.class))),
+      @ApiResponse(responseCode = "400", description = "Invalid input data.",
+          content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
   @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
       consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<TestimonialResponse> create(
-      @Valid @RequestBody CreateTestimonialRequest createTestimonialRequest) {
-
+      @Valid @RequestBody @io.swagger.v3.oas.annotations.parameters.RequestBody(required = true,
+      description = "New testimonial to create", content = @Content(
+          schema = @Schema(implementation = CreateTestimonialRequest.class)))
+          CreateTestimonialRequest createTestimonialRequest) {
     TestimonialResponse testimonialResponse = createTestimonialService.create(
         createTestimonialRequest);
 
@@ -57,10 +81,22 @@ public class TestimonialResource {
 
   }
 
+  @Operation(summary = "Update Testimonial.", description = "Update testimonial", tags = "Put")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Returns testimonial update.", content =
+      @Content(schema = @Schema(implementation = TestimonialResponse.class))),
+      @ApiResponse(responseCode = "400", description = "Invalid input data.", content =
+      @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "Testimonial not found.", content =
+      @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
   @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<TestimonialResponse> update(@PathVariable Long id,
-      @Valid @RequestBody UpdateTestimonialRequest updateTestimonialRequest) {
+  public ResponseEntity<TestimonialResponse> update(
+      @PathVariable Long id, @Valid @RequestBody @io.swagger.v3.oas.annotations.parameters
+      .RequestBody(required = true, description = "Testimonial Update", content = @Content(
+          schema = @Schema(implementation = UpdateTestimonialRequest.class)))
+          UpdateTestimonialRequest updateTestimonialRequest) {
     return ResponseEntity.ok().body(updateTestimonialService.update(id, updateTestimonialRequest));
   }
 }
