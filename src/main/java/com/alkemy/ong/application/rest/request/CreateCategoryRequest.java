7fa5374e8e236971aa99eb1bac22c97cdafcccbd
Kayki <jair.garcia.montano@gmail.com>
2022-04-14 15:41:45 -0300

    OT166-85: Documentation for categories added

--- src/main/java/com/alkemy/ong/application/rest/request/CreateCategoryRequest.java
+++ src/main/java/com/alkemy/ong/application/rest/request/CreateCategoryRequest.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.application.rest.request;
 
 import com.alkemy.ong.application.util.RegExpressionUtils;
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import lombok.Builder;
@@ -15,10 +16,13 @@ public class CreateCategoryRequest {
   @NotEmpty(message = "The name must not be empty or null.")
   @Pattern(regexp = RegExpressionUtils.ALPHABETIC_CHARACTERS_WITH_BLANK_SPACES,
       message = "The name has an invalid format.")
+  @Schema(example = "News", required = true)
   private String name;
 
+  @Schema(example = "All the latest news from my organization")
   private String description;
 
+  @Schema(example = "https://s3.com/mycategory.jpg")
   private String image;
 
 }
