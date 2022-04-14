846a3316179d92cbf728191fa95865322265d4dc
AlfredRdz <71676643+AlfredRdz@users.noreply.github.com>
2022-04-14 09:37:08 -0500

    OT166-58: update slides endpoint (#84)

--- src/main/java/com/alkemy/ong/infrastructure/spring/config/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/spring/config/security/SecurityConfig.java
@@ -107,6 +107,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .hasRole(Role.ADMIN.name())
         .antMatchers(HttpMethod.GET, "/slides")
         .hasRole(Role.ADMIN.name())
+        .antMatchers(HttpMethod.POST, "/slider/{id:^\\d+$}")
+        .hasRole(Role.ADMIN.name())
         .antMatchers(HttpMethod.DELETE, "/categories/{id:^\\d+$}")
         .hasRole(Role.ADMIN.name())
         .antMatchers(HttpMethod.DELETE, "/comments/{id:^\\d+$}")
