740ce4a9f1e54b05bfb0e1d8995e6fdfe36ea8c1
Kayki <jair.garcia.montano@gmail.com>
2022-04-14 12:54:13 -0300

    OT166-75: Service listComments added

---
+++ src/main/java/com/alkemy/ong/application/service/abstraction/IGetCommentService.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.application.service.abstraction;
+
+import com.alkemy.ong.application.rest.response.ListCommentsResponse;
+
+public interface IGetCommentService {
+
+  ListCommentsResponse listComments();
+}
