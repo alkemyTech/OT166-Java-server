3895a87c9f4665ee3ea7596a8e713a323e93a8df
AlfredRdz <alfredo12_1998@hotmail.com>
2022-04-18 16:53:27 -0500

    OT166-92: test update organization

--- src/test/java/com/alkemy/ong/bigtest/organization/UpdatePublicOrganizationDetailsIntegrationTest.java
+++ src/test/java/com/alkemy/ong/bigtest/organization/UpdatePublicOrganizationDetailsIntegrationTest.java
@@ -1,9 +1,12 @@
 package com.alkemy.ong.bigtest.organization;
 
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -11,6 +14,8 @@ import com.alkemy.ong.application.rest.request.UpdateOrganizationRequest;
 import com.alkemy.ong.bigtest.util.BigTest;
 import com.alkemy.ong.infrastructure.database.entity.OrganizationEntity;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.jayway.jsonpath.JsonPath;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import org.junit.Test;
 import org.springframework.http.HttpHeaders;
@@ -20,14 +25,14 @@ public class UpdatePublicOrganizationDetailsIntegrationTest extends BigTest {
 
   @Test
   public void shouldUpdateOrganizationWhenUserHasAdminRole() throws Exception {
-    OrganizationEntity ramdomOrganization = getRamdomOrganization();
+    saveOrganizationDetails();
+    OrganizationEntity ramdomOrganization = organizationRepository.findAll().get(0);
     Long randomOrganizationId = ramdomOrganization.getId();
 
     mockMvc.perform(patch("/organization/public", String.valueOf(randomOrganizationId))
             .content(getContent("Somos Mas", "Elm Street 3", "+5411345678", "somos.mas@ong.com"))
             .contentType(MediaType.APPLICATION_JSON)
             .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser()))
-        //.andExpect(jsonPath("$.id", notNullValue()))
         .andExpect(jsonPath("$.name", equalTo("Somos Mas")))
         .andExpect(jsonPath("$.address", equalTo("Elm Street 3")))
         .andExpect(jsonPath("$.phone", equalTo("+5411345678")))
@@ -35,23 +40,28 @@ public class UpdatePublicOrganizationDetailsIntegrationTest extends BigTest {
         .andExpect(status().isOk());
 
     assertOrganizationHasBeenUpdated(randomOrganizationId);
-    cleanOrganizationData(ramdomOrganization);
+  }
+
+  @Test
+  public void shouldReturnForbiddenErrorResponseWhenUserHasStandardRole() throws Exception {
+    mockMvc.perform(patch("/organization/public")
+            .contentType(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser()))
+        .andExpect(jsonPath("$.statusCode", equalTo(403)))
+        .andExpect(jsonPath("$.message",
+            equalTo("Access denied. Please, try to login again or contact your admin.")))
+        .andExpect(status().isForbidden());
   }
 
   @Test
   public void shouldReturnForbiddenErrorResponseWhenTokenIsNotSent() throws Exception {
-    OrganizationEntity ramdomOrganization = getRamdomOrganization();
-    Long randomOrganizationId = ramdomOrganization.getId();
-
-    mockMvc.perform(patch("/organization/public", String.valueOf(randomOrganizationId))
+    mockMvc.perform(patch("/organization/public")
             .content(getContent("Somos Mas", "Elm Street 3", "+5411345678", "somos.mas@ong.com"))
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$.statusCode", equalTo(403)))
         .andExpect(jsonPath("$.message",
             equalTo("Access denied. Please, try to login again or contact your admin.")))
         .andExpect(status().isForbidden());
-
-    cleanOrganizationData(ramdomOrganization);
   }
 
   private String getContent(String name, String address, String phone, String email)
@@ -72,7 +82,5 @@ public class UpdatePublicOrganizationDetailsIntegrationTest extends BigTest {
     assertEquals("Elm Street 3", optionalOrganizationEntityEntity.get().getAddress());
     assertEquals("+5411345678", optionalOrganizationEntityEntity.get().getPhone());
     assertEquals("somos.mas@ong.com", optionalOrganizationEntityEntity.get().getEmail());
-    cleanOrganizationData(optionalOrganizationEntityEntity.get());
   }
-
 }
